1e8bab9709b4a407c53e3daa39657e953b78d4d4
Imre Piller <piller.imre@gmail.com>
2017-05-23 20:42:44 +0200

    Logger interface

--- grimoire.py
+++ grimoire.py
@@ -3,6 +3,7 @@ Interface for the tagging application
 """
 
 from database import Database
+from logger import Logger
 
 
 class Grimoire(Database):
@@ -10,3 +11,6 @@ class Grimoire(Database):
 
     def __init__(self):
         super(Grimoire, self).__init__()
+        self._logger = Logger('/tmp/grimoire.log')
+        self._logger.restore_database(self)
+
---
+++ logger.py
@@ -0,0 +1,19 @@
+"""
+Manage the log file
+"""
+
+
+class Logger(object):
+    """Log file manager"""
+
+    def __init__(self, path):
+        """Set the path of the log file."""
+        self._path = path
+
+    def save_operation(self, **operation):
+        """Save the operation to the log file."""
+        pass
+
+    def restore_database(self, database):
+        """Restore the database contents from the log file."""
+        pass
